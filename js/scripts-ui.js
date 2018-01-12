d98c5d903058b9a46ed166dd0d5ea40e270a736c
Griffin P Longtin <griflongtin@live.com>
2018-01-12 13:59:21 -0800

    add a callAPI function with a callback function

--- js/scripts-ui.js
+++ js/scripts-ui.js
@@ -1,5 +1,10 @@
-import { DoctorAPIobj } from './../src/js/scripts.js';
+import { DoctorAPIobj } from './../js/scripts.js';
 const apiKey = require('./../.env').apiKey;
 
 
 const user = new DoctorAPIobj("back%20pain", 'or-portland', apiKey);
+
+user.callAPI(user.url, function(data){
+  console.log(data, '2');
+
+});
