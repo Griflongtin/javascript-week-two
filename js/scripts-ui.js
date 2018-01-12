594ab13fe92172c4e9d1c73db964cf6644faa1eb
Griffin P Longtin <griflongtin@live.com>
2018-01-12 13:40:44 -0800

    add API to the OBJ and the .env

---
+++ js/scripts-ui.js
@@ -0,0 +1,5 @@
+import { DoctorAPIobj } from './../src/js/scripts.js';
+const apiKey = require('./../.env').apiKey;
+
+
+const user = new DoctorAPIobj("back%20pain", 'or-portland', apiKey);
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 export class DoctorAPIobj {
-  constructor(simtom, place) {
+  constructor(simtom, place, apiKey) {
     this.simtom = simtom;
     this.place = place;
+    this.apiKey = apiKey;
+    this.url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${this.simtom}&location=${this.place}&skip=0&limit=10&user_key=${this.apiKey}`
   }
+
+  
 
 }
