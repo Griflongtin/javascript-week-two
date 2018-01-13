6c900c68b81ae8ba7dfb6023f5b9a924ad2f4e2b
Griffin P Longtin <griflongtin@live.com>
2018-01-12 17:54:23 -0800

    add address to the profile append

--- js/scripts-ui.js
+++ js/scripts-ui.js
@@ -13,7 +13,7 @@ $(function() {
 
     data.then(function(data){
       const doc = JSON.parse(data);
-      console.log(doc.data[0].profile);
+      console.log(doc.data[0].practices[0].visit_address.city);
       console.log(doc.data);
       for (var i = 0; i < 10; i++) {
         $(".output").append(`<ul>
@@ -21,8 +21,16 @@ $(function() {
           <li>Name:${doc.data[i].profile.first_name} ${doc.data[i].profile.last_name}</li>
           <img src="${doc.data[i].profile.image_url}" alt="${doc.data[i].profile.first_name} ${doc.data[i].profile.last_name}">
           <li>Gender: ${doc.data[i].profile.gender}</li>
+          <li>Office: ${doc.data[i].practices[0].name}</li>
+          <ul>
+            <li>Address:</li>
+            <li>City: ${doc.data[i].practices[0].visit_address.city}</li>
+            <li>State: ${doc.data[i].practices[0].visit_address.state_long}</li>
+            <li>Street: ${doc.data[i].practices[0].visit_address.street}</li>
+            <li>Zip: ${doc.data[i].practices[0].visit_address.zip}</li>
+          </ul>
           <li>BIO: ${doc.data[i].profile.bio}</li>
-          </ul>`)
+          </ul>`);
       }
     });
   });
--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ export class DoctorAPIobj {
     this.symptoms = symptoms;
     this.place = place;
     this.apiKey = apiKey;
-    this.url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${this.symptoms}&location=${this.place}&skip=0&limit=10&user_key=${this.apiKey}`
+    this.url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${this.symptoms}&location=${this.place}&skip=0&limit=10&user_key=${this.apiKey}`;
   }
 
   callAPI(input) {
@@ -17,7 +17,7 @@ export class DoctorAPIobj {
         } else {
           error(Error(request));
         }
-      }
+      };
       request.open('GET', url);
       request.send();
     });
